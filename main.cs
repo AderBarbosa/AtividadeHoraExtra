210ef882bb96aeb0815656f165155aad773c5416
Ader Barbosa <119372914+AderBarbosa@users.noreply.github.com>
2023-02-06 23:53:32 +0000

    Finalizado

--- main.cs
+++ main.cs
@@ -2,9 +2,12 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-  string conver, conver1;
-    Double hora, valor, horasex, extra, salario, i;
+  string conver, conver1, convert2;
+    Double hora, valor, extra, salario, salarioat;
 
+  Console.WriteLine("Informe seu salário bruto: ");  
+  convert2 = Console.ReadLine ();
+  salario = Double.Parse(convert2);
   Console.WriteLine("Informe o valor de uma hora trbalhada: ");
   conver = Console.ReadLine ();
   valor = Double.Parse(conver);
@@ -13,14 +16,14 @@ class Program {
   hora = Double.Parse(conver1);
   if (hora > 160)
     {
-  horasex = (hora - 160);
-    extra = ((horasex * valor) * 1.5);
-    salario = extra + (valor * 160);
-    Console.WriteLine(" O total de horas extra é de " + horasex + " e o valor do salário total é de " + salario);
+    hora = (hora - 160);
+    extra = ((valor * hora) * 2);
+    salarioat = (extra + salario);
+    Console.WriteLine(" O total de horas extra trabalhadas é de " + extra + " e o valor do salário total é de " + salarioat);
     }
     else
     {
-      salario = (valor * 160);Console.WriteLine(" O funcionário não realizou nenhuma hora extra, portanto seu salário é de: " + salario);
+    Console.WriteLine(" O funcionário não realizou nenhuma hora extra, portanto seu salário é de: " + salario);
     }
     }
   }
